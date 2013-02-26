150d7f3b73ad5400f8bb4f48eb93505f2a4aa142
Michael Baudino <michael.baudino@alpine-lab.com>
2013-02-26 23:21:25 +0100

    Changed command-line parser to manage multiple files

--- addic7ed.rb
+++ addic7ed.rb
@@ -14,12 +14,14 @@ VERSION="0.0.3"
 
 options = {}
 OptionParser.new do |opts|
+  opts.banner = "Usage: addic7ed.rb [options] <file1> [<file2>, <file3>, ...]"
+
   opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
     options[:verbose] = v
   end
 
-  opts.on("-f [FILENAME]", "--filename [FILENAME]", "File to look subtitles for") do |f|
-    options[:filename] = f
+  opts.on("-l [LANGUAGE]", "--language [LANGUAGE]", "Language to look subtitles for") do |l|
+    options[:language] = l
   end
 
   opts.on_tail("-h", "--help", "Show this message") do
@@ -33,27 +35,36 @@ OptionParser.new do |opts|
   end
 end.parse!
 
+options[:filenames] = ARGV
+
 puts "Verbose:  #{options[:verbose]}"
-puts "Filename: #{options[:filename]}"
+puts "Filenames: #{options[:filenames]}"
+puts "Language: #{options[:language]}"
 
-# begin
-#   file = Addic7ed::Filename.new(filename)
-#   puts file
-# rescue Exception => e
-#   puts "Error: #{e.message}"
-#   puts 'Aborting.'
-# end
+options[:filenames].each do |filename|
+  unless File.file? filename
+    puts "Warning: #{filename} does not exist or is not a regular file. Skipping."
+  end
 
-# shows_ids = {}
-# seasons_ids = {}
-# episodes_ids = {}
+  # begin
+  #   file = Addic7ed::Filename.new(filename)
+  #   puts file
+  # rescue Exception => e
+  #   puts "Error: #{e.message}"
+  #   puts 'Aborting.'
+  # end
 
-# shows_url = "http://www.addic7ed.com/ajax_getShows.php"
-# seasons_url = "http://www.addic7ed.com/ajax_getSeasons.php"
-# episodes_url = "http://www.addic7ed.com/ajax_getEpisodes.php"
+  # shows_ids = {}
+  # seasons_ids = {}
+  # episodes_ids = {}
 
-# # Fetch shows list
-# Nokogiri::HTML(open(shows_url)).css('option').each do |show_html|
-#   shows_ids[show_html.content] = show_html['value']
-# end
+  # shows_url = "http://www.addic7ed.com/ajax_getShows.php"
+  # seasons_url = "http://www.addic7ed.com/ajax_getSeasons.php"
+  # episodes_url = "http://www.addic7ed.com/ajax_getEpisodes.php"
 
+  # # Fetch shows list
+  # Nokogiri::HTML(open(shows_url)).css('option').each do |show_html|
+  #   shows_ids[show_html.content] = show_html['value']
+  # end
+
+end
