02758be05b26b280ae801c36448c97956666b01e
Michael Baudino <michael.baudino@alpine-lab.com>
2013-02-27 03:10:19 +0100

    Added tests for addic7ed-common

---
+++ spec/addic7ed-common_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+require './lib/addic7ed-common'
+
+describe Addic7ed do
+  it 'should define SHOWS_URL' do
+    Addic7ed::SHOWS_URL.should_not be_nil
+  end
+  it 'should define EPISODES_URL' do
+    Addic7ed::EPISODES_URL.should_not be_nil
+  end
+  it 'should define EPISODE_REDIRECT_URL' do
+    Addic7ed::EPISODE_REDIRECT_URL.should_not be_nil
+  end
+  it 'should define LANGUAGES' do
+    Addic7ed::LANGUAGES.should_not be_nil
+    Addic7ed::LANGUAGES['fr'].should == {name: 'French', id: 8}
+  end
+end
