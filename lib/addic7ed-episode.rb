18406a97c9fb8ea4f92cb1d43d2b65826e0c9169
Michael Baudino <michael.baudino@alpine-lab.com>
2013-02-28 03:11:00 +0100

    Splitted a function in 2 functions for code clarity. Not sure if it works :-/

--- lib/addic7ed-episode.rb
+++ lib/addic7ed-episode.rb
@@ -29,25 +29,35 @@ module Addic7ed
         sublist_node = doc.css('#container95m table.tabel95 table.tabel95')
         raise NoSubtitleFound if sublist_node.size == 0
         sublist_node.each do |sub_node|
-          begin
-            title_node = sub_node.css('.NewsTitle').first
-            title = title_node.content.gsub(/ \nVersion /, '').gsub(/,.*/, '')
-            language_node = sub_node.css('.language').first
-            language = language_node.content.gsub(/\A\W*/, '').gsub(/[^\w\)]*\z/, '')
-            raise WTFError.new("We're asking for #{LANGUAGES[lang][:name].capitalize} subtitles and Addic7ed gives us #{language.capitalize} subtitles") if LANGUAGES[lang][:name].downcase != language.downcase
-            status_node = sub_node.css('tr:nth-child(3) td:nth-child(4) b').first
-            status = status_node.content.strip
-            url_node = sub_node.css('a.buttonDownload').first
-            url = 'http://www.addic7ed.com' + url_node['href']
-            downloads_node = sub_node.css('tr:nth-child(4) td.newsDate').first
-            downloads = /(?<downloads>\d*) Downloads/.match(downloads_node.content)[:downloads]
-            @subtitles[lang] << Addic7ed::Subtitle.new(title, language, status, url, downloads)
-          rescue
-            raise ParsingError
-          end
+          @subtitles[lang] << parse_subtitle_node(sub_node, lang)
         end
       end
       @subtitles[lang]
+    end
+
+    def best_subtitle(lang = 'fr')
+      # TODO
+    end
+
+    protected
+
+    def parse_subtitle_node(sub_node, lang)
+      begin
+        title_node = sub_node.css('.NewsTitle').first
+        title = title_node.content.gsub(/ \nVersion /, '').gsub(/,.*/, '')
+        language_node = sub_node.css('.language').first
+        language = language_node.content.gsub(/\A\W*/, '').gsub(/[^\w\)]*\z/, '')
+        raise WTFError.new("We're asking for #{LANGUAGES[lang][:name].capitalize} subtitles and Addic7ed gives us #{language.capitalize} subtitles") if LANGUAGES[lang][:name].downcase != language.downcase
+        status_node = sub_node.css('tr:nth-child(3) td:nth-child(4) b').first
+        status = status_node.content.strip
+        url_node = sub_node.css('a.buttonDownload').first
+        url = 'http://www.addic7ed.com' + url_node['href']
+        downloads_node = sub_node.css('tr:nth-child(4) td.newsDate').first
+        downloads = /(?<downloads>\d*) Downloads/.match(downloads_node.content)[:downloads]
+        Addic7ed::Subtitle.new(title, language, status, url, downloads)
+      rescue
+        raise ParsingError
+      end
     end
 
   end
