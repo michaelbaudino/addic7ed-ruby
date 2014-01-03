6ba03fbcf22f2f9c807fb701d0b872b8b641441d
Michael Baudino <michael.baudino@alpine-lab.com>
2014-01-03 18:36:31 +0100

    Minor refactor to match Ruby coding styleguides

--- lib/addic7ed/errors.rb
+++ lib/addic7ed/errors.rb
@@ -1,22 +1,12 @@
 module Addic7ed
-  class InvalidFilename < StandardError
-  end
-  class ShowNotFound < StandardError
-  end
-  class EpisodeNotFound < StandardError
-  end
-  class LanguageNotSupported < StandardError
-  end
-  class ParsingError < StandardError
-  end
-  class NoSubtitleFound < StandardError
-  end
-  class DownloadError < StandardError
-  end
-  class DownloadLimitReached < StandardError
-  end
-  class SubtitleCannotBeSaved < StandardError
-  end
-  class HTTPError < StandardError
-  end
+  class InvalidFilename < StandardError; end
+  class ShowNotFound < StandardError; end
+  class EpisodeNotFound < StandardError; end
+  class LanguageNotSupported < StandardError; end
+  class ParsingError < StandardError; end
+  class NoSubtitleFound < StandardError; end
+  class DownloadError < StandardError; end
+  class DownloadLimitReached < StandardError; end
+  class SubtitleCannotBeSaved < StandardError; end
+  class HTTPError < StandardError; end
 end
