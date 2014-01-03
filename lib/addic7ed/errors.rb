e3f8b2479426cdcf0d9cd0bebac3612d31b6ce7c
Michael Baudino <michael.baudino@alpine-lab.com>
2014-01-03 19:09:53 +0100

    Even more Ruby-ish and DRY declaration of exceptions (related to 6ba03fbcf22f2f9c807fb701d0b872b8b641441d)

--- lib/addic7ed/errors.rb
+++ lib/addic7ed/errors.rb
@@ -1,12 +1,9 @@
 module Addic7ed
-  class InvalidFilename < StandardError; end
-  class ShowNotFound < StandardError; end
-  class EpisodeNotFound < StandardError; end
-  class LanguageNotSupported < StandardError; end
-  class ParsingError < StandardError; end
-  class NoSubtitleFound < StandardError; end
-  class DownloadError < StandardError; end
-  class DownloadLimitReached < StandardError; end
-  class SubtitleCannotBeSaved < StandardError; end
-  class HTTPError < StandardError; end
+  exceptions = [
+    :InvalidFilename,       :ShowNotFound,    :EpisodeNotFound, :LanguageNotSupported,
+    :ParsingError,          :NoSubtitleFound, :DownloadError,   :DownloadLimitReached,
+    :SubtitleCannotBeSaved, :HTTPError
+  ]
+
+  exceptions.each { |e| const_set(e, Class.new(StandardError)) }
 end
