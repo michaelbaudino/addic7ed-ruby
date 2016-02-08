99523d8af2f6297045b6a6e80408608bcd4540e3
Michael Baudino <michael.baudino@alpine-lab.com>
2016-02-08 08:42:18 +0400

    Typos in ShowList specs

--- spec/lib/addic7ed/show_list_spec.rb
+++ spec/lib/addic7ed/show_list_spec.rb
@@ -3,7 +3,7 @@ require "./lib/addic7ed"
 
 describe Addic7ed::ShowList, ".find" do
   it "changes all spaces to underscores" do
-    expect(Addic7ed::ShowList.find("The Walking Dead")).to eq "The_Walking_Dead"
+    expect(Addic7ed::ShowList.find("The.Walking.Dead")).to eq "The_Walking_Dead"
   end
 
   it "wraps country code with parenthesis" do
@@ -11,11 +11,11 @@ describe Addic7ed::ShowList, ".find" do
     expect(Addic7ed::ShowList.find("Vikings.UK")).to eq "Vikings_(UK)"
   end
 
-  it "detects country code even in lowercase" do
+  it "is case-insensitive" do
     expect(Addic7ed::ShowList.find("shameless.us")).to eq "Shameless_(US)"
   end
 
-  it "sets production year between parenthesis" do
+  it "wraps production year with parenthesis" do
     expect(Addic7ed::ShowList.find("Vikings.2013")).to eq "Vikings_(2013)"
   end
 
@@ -28,6 +28,6 @@ describe Addic7ed::ShowList, ".find" do
   end
 
   it "raises a ShowNotFound error when no matching show is found" do
-    expect{Addic7ed::ShowList.find("Not an existing show")}.to raise_error Addic7ed::ShowNotFound
+    expect{Addic7ed::ShowList.find("Not.an.existing.show")}.to raise_error Addic7ed::ShowNotFound
   end
 end
