5c3bcb2546637faebad05be9dadf806b53b73d91
Michael Baudino <michael.baudino@alpine-lab.com>
2016-06-19 12:02:23 +0400

    Require files more smartly

--- lib/addic7ed.rb
+++ lib/addic7ed.rb
@@ -1,14 +1,5 @@
-require 'addic7ed/version'
-require 'addic7ed/common'
-require 'addic7ed/service'
-require 'addic7ed/services/get_shows_list'
-require 'addic7ed/services/normalize_version'
-require 'addic7ed/services/normalize_comment'
-require 'addic7ed/services/url_encode_show_name'
-require 'addic7ed/services/parse_subtitle'
-require 'addic7ed/services/parse_page'
-require 'addic7ed/services/download_subtitle'
-require 'addic7ed/errors'
-require 'addic7ed/models/video_file'
-require 'addic7ed/models/episode'
-require 'addic7ed/models/subtitle'
+Dir[
+  File.expand_path("lib/addic7ed/*.rb"),
+  File.expand_path("lib/addic7ed/services/**/*.rb"),
+  File.expand_path("lib/addic7ed/models/**/*.rb")
+].each { |file| require file }
