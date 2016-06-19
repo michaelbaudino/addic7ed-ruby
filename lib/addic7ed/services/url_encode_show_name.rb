8097d68d0b2d29d27997f318710352eb1b1220f2
Michael Baudino <michael.baudino@alpine-lab.com>
2016-06-19 11:55:06 +0400

    Refactor URLEncodeShowName to be more readable

--- lib/addic7ed/services/url_encode_show_name.rb
+++ lib/addic7ed/services/url_encode_show_name.rb
@@ -8,43 +8,35 @@ module Addic7ed
       @filename = filename
     end
 
-    # This method is unfortunately over complex because we have to compare the given show name to
+    # This service is unfortunately over complex because we have to compare the given show name to
     # the actual Addic7ed shows list in order to find out how Addic7ed URL-encodes this show name
     # (this is due to the inconsistency of their URL-encoding policy)
     def call
-      shows_matching = shows_matching_exactly
-      shows_matching = shows_matching_without_year if shows_matching.empty?
-      raise ShowNotFound if shows_matching.empty?
-      shows_matching.last.gsub(' ', '_')
+      matching_shows = matching_shows(ignore_year: false)
+      matching_shows = matching_shows(ignore_year: true) if matching_shows.empty?
+      raise ShowNotFound if matching_shows.empty?
+      matching_shows.last.gsub(' ', '_')
     end
 
   private
 
-    def shows_matching_exactly
-      @shows_matching_exactly ||= addic7ed_shows.select{ |addic7ed_show| is_matching? addic7ed_show }
+    def matching_shows(opts)
+      addic7ed_shows.select{ |show_name| is_matching?(show_name, opts) }
     end
 
-    def shows_matching_without_year
-      @shows_matching_without_year ||= addic7ed_shows.select{ |addic7ed_show| is_matching? addic7ed_show, :comparer_without_year }
+    def normalize(show_name, opts)
+      show_name
+        .downcase
+        .gsub("'", "")
+        .gsub(/[_\.]+/, ' ')
+        .gsub(/ (US|UK)( |$)/i, ' (\1)\2')
+        .gsub(/ (\d{4})( |$)/i, ' (\1)\2')
+        .strip
+        .tap { |show_name| show_name.gsub!(/ \(\d{4}\)( |$)/, '\1') if opts[:ignore_year] }
     end
 
-    def default_comparer(showname)
-      showname.downcase.gsub("'", "").gsub(".", " ").strip
-    end
-
-    def comparer_without_year(showname)
-      default_comparer(showname).gsub(/ \(\d{4}\)( |$)/, '\1')
-    end
-
-    def is_matching?(addic7ed_show, comparer = :default_comparer)
-      [humanized_name, addic7ed_show].map(&method(comparer)).reduce(:==)
-    end
-
-    def humanized_name
-      @humanized_name ||= filename.
-                            gsub(/[_\.]+/, ' ').
-                            gsub(/ (US|UK)( |$)/i, ' (\1)\2').
-                            gsub(/ (\d{4})( |$)/i, ' (\1)\2')
+    def is_matching?(addic7ed_show, opts)
+      normalize(addic7ed_show, opts) == normalize(filename, opts)
     end
 
     def addic7ed_shows
